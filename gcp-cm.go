4f6cf36c155f1487fece42b3ebe53a328302136f
aurorali <aurora.li615@gmail.com>
2023-03-16 14:13:33 +0000

    initial commit for gcp cert manager

---
+++ gcp-cm.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/genproto/protobuf/field_mask"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	// "cloud.google.com/go"
+	certificatemanager "cloud.google.com/go/certificatemanager/apiv1"
+	certificatemanagerpb "cloud.google.com/go/certificatemanager/apiv1/certificatemanagerpb"
+)
+
+// CreateGCPSession will connect to GCP with the account's credentials
+func CreateGCPSession(roleArn string, region string){
+	l := log.WithFields(
+		log.Fields{
+			"action": "CreateGCPSession",
+		},
+	)
+	l.Print("CreateGCPSession")
+	if region == "" {
+		region = os.Getenv("GCP_REGION")
+	}
+	if region == "" {
+		region = "us-central1"
+	}
+	// cfg := &GCP.Config{
+	// 	Region: GCP.String(region),
+	// }
+	// sess, err := session.NewSession(cfg)
+	// reqId := uuid.New().String()
+	// if roleArn != "" {
+	// 	l.Printf("CreateGCPSession roleArn=%s requestId=%s", roleArn, reqId)
+	// 	creds := stscreds.NewCredentials(sess, roleArn, func(p *stscreds.AssumeRoleProvider) {
+	// 		p.RoleSessionName = "cert-manager-sync-" + reqId
+	// 	})
+	// 	cfg.Credentials = creds
+	// }
+	// if err != nil {
+	// 	l.Printf("%+v", err)
+	// }
+	// return sess, cfg, nil
+}
+
+
+
+
+// GCPCerts accepts a slice of Secrets and returns only those configured
+// for replication to GCP
+func GCPCerts(s []corev1.Secret) []corev1.Secret {
+	var ac []corev1.Secret
+	for _, v := range s {
+		if v.Annotations[operatorName+"/GCP-enabled"] == "true" && cacheChanged(v) {
+			ac = append(ac, v)
+		}
+	}
+	return ac
+}
+
+// secretToGCPInput converts a k8s secret to a properly-formatted GCP Import object
+func secretToGCPCert(s corev1.Secret, project string, location string, gcp_cert_name string) (*certificatemanagerpb.Certificate, error) {
+
+
+	sm_cert := &certificatemanagerpb.Certificate_SelfManagedCertificate{
+		PemCertificate: string(s.Data["tls.crt"]),
+		PemPrivateKey: string(s.Data["tls.key"]),
+	}
+
+
+	if gcp_cert_name =="" {
+		gcp_cert_name = s.ObjectMeta.Namespace+"-" + s.ObjectMeta.Name
+	}
+
+	return &certificatemanagerpb.Certificate{
+			Name: "projects/"+project+"/locations/" + location + "certificates/" + gcp_cert_name,
+			Type: &certificatemanagerpb.Certificate_SelfManaged{SelfManaged: sm_cert},}, nil
+
+
+}
+
+// handleGCPCerts handles the sync of all GCP-enabled certs
+func handleGCPCerts(ss []corev1.Secret) {
+	ss = GCPCerts(ss)
+	l := log.WithFields(
+		log.Fields{
+			"action": "handleGCPCerts",
+		},
+	)
+	l.Print("handleGCPCerts")
+	l.Print("connecting to gcp")
+	ctx := context.Background()
+	// This snippet has been automatically generated and should be regarded as a code template only.
+	// It will require modifications to work:
+	// - It may require correct/in-range values for request initialization.
+	// - It may require specifying regional endpoints when creating the service client as shown in:
+	//   https://pkg.go.dev/cloud.google.com/go#hdr-Client_Options
+	c, err := certificatemanager.NewClient(ctx)
+	if err != nil {
+		l.Print(err)
+	}
+	defer c.Close()
+	for i, s := range ss {
+		l.Debugf("processing secret %s (%d/%d)", s.ObjectMeta.Name, i+1, len(ss))
+		err := handleGCPCert(s,c,ctx)
+		if err != nil {
+			l.Printf("handleGCPCert error=%v", err)
+			continue
+		}
+		c := secretToCert(s)
+		addToCache(c)
+	}
+}
+
+// handleGCPCert handles the update of a single GCP Certificate
+func handleGCPCert(s corev1.Secret, c *certificatemanager.Client, ctx context.Context) error {
+	l := log.WithFields(
+		log.Fields{
+			"action": "handleGCPCert",
+			"name":   s.ObjectMeta.Name,
+		},
+	)
+	l.Print("handleGCPCert")
+	var project, location string
+	if s.ObjectMeta.Annotations[operatorName+"/GCP-project"] != "" {
+		project = s.ObjectMeta.Annotations[operatorName+"/GCP-project"]
+	} else {
+		project = os.Getenv("PROJECT_ID")
+	}
+
+	if s.ObjectMeta.Annotations[operatorName+"/GCP-location"] != "" {
+		location = s.ObjectMeta.Annotations[operatorName+"/GCP-location"]
+	} else {
+		location = os.Getenv("LOCATION")
+	}
+	ai, err := secretToGCPCert(s,project,location,s.ObjectMeta.Annotations[operatorName+"/GCP-certificate-name"])
+	if err != nil {
+		l.Print(err)
+		return err
+	}
+
+	
+	
+	// secret already has an GCP GCP cert attached
+	if s.ObjectMeta.Annotations[operatorName+"/GCP-certificate-name"] != "" {
+		// this is our first time sending to GCP, tag
+			req := &certificatemanagerpb.UpdateCertificateRequest{
+				Certificate: ai,
+				UpdateMask: &field_mask.FieldMask{
+					Paths: []string{"type"},
+				},
+			}
+			op, err := c.UpdateCertificate(ctx,req)
+			if err != nil {
+				// TODO: Handle error.
+				l.Errorf("cannot create cert because of %s",err)
+				return err
+			}
+			resp, err := op.Wait(ctx)
+			if err != nil {
+				// TODO: Handle error.
+				l.Errorf("cannot complete creating cert because of %s",err)
+				return err
+			}
+			l.Print(resp)
+			l.Infof("Cert updated to CCM as %s", resp.Name)
+		} else {
+			req := &certificatemanagerpb.CreateCertificateRequest{
+				// TODO: Fill request struct fields.
+				// See https://pkg.go.dev/google.golang.org/genproto/googleapis/cloud/certificatemanager/v1#CreateCertificateRequest.
+				Parent: "projects/"+project+"/locations/" + location,
+				CertificateId: s.ObjectMeta.Namespace+"-" + s.ObjectMeta.Name,
+				Certificate: ai,}
+			op, err := c.CreateCertificate(ctx, req)
+				if err != nil {
+					// TODO: Handle error.
+					l.Errorf("cannot create cert because of %s",err)
+					return err
+				}
+			resp, err := op.Wait(ctx)
+				if err != nil {
+					// TODO: Handle error.
+					l.Errorf("cannot complete creating cert because of %s",err)
+					return err
+				}
+			l.Print(resp)
+			s.ObjectMeta.Annotations[operatorName+"/GCP-certificate-name"] = resp.Name
+			l.Infof("Cert uploaded to CCM as %s", resp.Name)
+	}
+	sc := k8sClient.CoreV1().Secrets(os.Getenv("SECRETS_NAMESPACE"))
+	uo := metav1.UpdateOptions{}
+	_, uerr := sc.Update(
+		context.Background(),
+		&s,
+		uo,
+	)
+	if uerr != nil {
+		l.Print(uerr)
+		return uerr
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,16 +3,22 @@ module github.com/robertlestak/cert-manager-sync
 go 1.18
 
 require (
+	cloud.google.com/go v0.107.0
+	cloud.google.com/go/certificatemanager v1.6.0
 	github.com/aws/aws-sdk-go v1.44.13
 	github.com/google/uuid v1.3.0
 	github.com/hashicorp/vault/api v1.5.0
 	github.com/sirupsen/logrus v1.8.1
+	google.golang.org/genproto v0.0.0-20230209215440-0dfe4f8abfcc
 	k8s.io/api v0.24.0
 	k8s.io/apimachinery v0.24.0
 	k8s.io/client-go v0.24.0
 )
 
 require (
+	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/longrunning v0.3.0 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/armon/go-metrics v0.3.9 // indirect
@@ -26,10 +32,14 @@ require (
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-hclog v0.16.2 // indirect
@@ -67,22 +77,23 @@ require (
 	github.com/pierrec/lz4 v2.5.2+incompatible // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
-	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/oauth2 v0.5.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
+	google.golang.org/api v0.110.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20210402141018-6c239bbf2bb1 // indirect
-	google.golang.org/grpc v1.41.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/klog/v2 v2.60.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20220328201542-3ee0da9b0b42 // indirect
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
